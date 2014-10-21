f9fbec79e3cde65a51e443425d62c5432e9bae49
Emett Speer <speer.emett@gmail.com>
2014-10-20 20:54:07 -0700

    Finished adding comments and added a display details method.

--- Class5/Lab4/Model.java
+++ Class5/Lab4/Model.java
@@ -26,7 +26,12 @@ public class Model
     /**
      * Overleaded Constructor
      * 
-     * @param  y   a sample parameter for a method
+     * @param _firstName used to initiate first name field as a string
+     * @param _lastName used to initiate last name field as a string
+     * @param _height used to initiate the height field as a int
+     * @param _weight used to initiate the weight field as a double
+     * @param _canTravel used to initiate if the model can travel as a boolean
+     * @param _smokes used to initiate if the model smokes as a boolean
      */
     public Model(String _firstName, String _lastName, int _height, double _weight,
                     boolean _canTravel, boolean _smokes)
@@ -35,10 +40,13 @@ public class Model
         setLastName(_lastName);
         setHeight(_height);
         setWeight(_weight);
-        canTravel = _canTravel;
-        smokes = _smokes;
+        setCanTravel(_canTravel);
+        setSmokes(_smokes);
     }
     
+    /**
+     * @param _height used to set the models height as a int
+     */
     public void setHeight(int _height)
     {
         if (_height >= 0) {
@@ -48,6 +56,9 @@ public class Model
         }
     }
 
+    /**
+     * @param _weight used to set the models weight as a double
+     */
     public void setWeight(double _weight)
     {
         if (_weight >= 0) {
@@ -57,6 +68,9 @@ public class Model
         }      
     }
 
+    /**
+     * @param _firstName used to set the models first name as a string
+     */
     public void setFirstName(String _firstName)
     {
         if (!_firstName.isEmpty() && _firstName != null) {
@@ -66,6 +80,9 @@ public class Model
         }
     }
 
+    /** 
+     * @param _lastName used to set the models last name as a string
+     */
     public void setLastName(String _lastName)
     {
         if(!_lastName.isEmpty() && _lastName != null) {
@@ -75,42 +92,76 @@ public class Model
         }
     }
 
+    /**
+     * @param _canTravel used to set the models ability to travel as a boolean
+     */
     public void setCanTravel(boolean _canTravel)
     {
         canTravel = _canTravel;
     }
 
+    /**
+     * @param _smokes used to set if the model smokes as a boolean
+     */
     public void setSmokes(boolean _smokes)
     {
         smokes = _smokes;
     }
 
+    /**
+     * @return Gets the first name of the model as a string
+     */
     public String getFirstName()
     {
         return firstName;
     }
 
+    /**
+     * @return Gets the last name of the model as a string
+     */
     public String getLastName()
     {
         return lastName;
     }
 
+    /**
+     * @return Gets the height of the model as a int
+     */
     public int getHeight()
     {
         return height;
     }
 
+    /** 
+     * @return Gets the weight of the model as a double
+     */
     public double getWeight()
     {
         return weight;
     }
 
+    /**
+     * @return Gets if the model can travel or not as a boolean
+     */
     public boolean isCanTravel()
     {
         return canTravel;
     }
 
+    /**
+     * @return Gets if the model smokes or not as a boolean
+     */
     public boolean isSomkes()
     {
         return smokes;
     }
+    
+    public void getModelDetails()
+    {
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Travels: " + canTravel);
+        System.out.println("Somkes: " + smokes);
+    }
+}
