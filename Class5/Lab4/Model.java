6d4f4bfc0f67e898cb3b93eb225f12a848822f4a
Emett Speer <speer.emett@gmail.com>
2014-10-20 20:02:49 -0700

    Adding Class for Model and fields

---
+++ Class5/Lab4/Model.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Model here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Model
+{
+    // instance variables - replace the example below with your own
+    private String firstName;
+    private String lastName;
+    private double height;
+    private double weight;
+    private boolean smokes;
+
+    /**
+     * Constructor for objects of class Model
+     */
+    public Model()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
