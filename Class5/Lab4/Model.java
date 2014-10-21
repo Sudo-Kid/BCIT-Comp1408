f241d72d430dd732afb4f88a1fc1b2bfce02551b
Emett Speer <speer.emett@gmail.com>
2014-10-20 20:37:04 -0700

    Finished logic for Model class. Need to still add comments.

--- Class5/Lab4/Model.java
+++ Class5/Lab4/Model.java
@@ -10,8 +10,9 @@ public class Model
     // instance variables - replace the example below with your own
     private String firstName;
     private String lastName;
-    private double height;
+    private int height;
     private double weight;
+    private boolean canTravel;
     private boolean smokes;
 
     /**
@@ -19,19 +20,97 @@ public class Model
      */
     public Model()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Overleaded Constructor
      * 
      * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public Model(String _firstName, String _lastName, int _height, double _weight,
+                    boolean _canTravel, boolean _smokes)
     {
-        // put your code here
-        return x + y;
+        setFirstName(_firstName);
+        setLastName(_lastName);
+        setHeight(_height);
+        setWeight(_weight);
+        canTravel = _canTravel;
+        smokes = _smokes;
     }
-}
+    
+    public void setHeight(int _height)
+    {
+        if (_height >= 0) {
+            height = _height;
+        } else {
+            System.out.println("Your value must not be less then 0");
+        }
+    }
+
+    public void setWeight(double _weight)
+    {
+        if (_weight >= 0) {
+            weight = _weight;
+        } else {
+            System.out.println("Your value must not be less then 0");
+        }      
+    }
+
+    public void setFirstName(String _firstName)
+    {
+        if (!_firstName.isEmpty() && _firstName != null) {
+            firstName = _firstName;
+        } else {
+            System.out.println("You must input a first name");
+        }
+    }
+
+    public void setLastName(String _lastName)
+    {
+        if(!_lastName.isEmpty() && _lastName != null) {
+            lastName = _lastName;
+        } else {
+            System.out.println("You must input a last name");
+        }
+    }
+
+    public void setCanTravel(boolean _canTravel)
+    {
+        canTravel = _canTravel;
+    }
+
+    public void setSmokes(boolean _smokes)
+    {
+        smokes = _smokes;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public double getWeight()
+    {
+        return weight;
+    }
+
+    public boolean isCanTravel()
+    {
+        return canTravel;
+    }
+
+    public boolean isSomkes()
+    {
+        return smokes;
+    }
